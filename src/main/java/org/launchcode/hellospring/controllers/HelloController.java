574f4e7de731a8f5f7f4c4a6cc12899bf451ed24
Artimas <mblanzy@hotmail.com>
2019-03-10 23:08:57 -0500

    added parameter handleing

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,7 +2,9 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,13 +19,41 @@ public class HelloController {
 
         String name = request.getParameter("name");
 
+        if (name == null) {
+            name = "World";
+        }
+
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "hello", method=RequestMethod.GET)
+    @ResponseBody
+    public String helloForm() {
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='greet me' />" +
+                "</form>";
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method=RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request) {
+        String name = request.getParameter("name");
+
+        return "Hello " + name;
+    }
+
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
         return "Hello " + name;
     }
 
     @RequestMapping(value = "goodbye")
-    @ResponseBody
     public String goodbye() {
-        return "Goodbye";
+        return "redirect:/";
     }
 
 }
